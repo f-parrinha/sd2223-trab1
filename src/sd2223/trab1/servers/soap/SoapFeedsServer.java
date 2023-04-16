bae8b623ebedd47c5fca5c55ba18ea983d3c7e8a
Martin <m.magdalinchev@campus.fct.unl.pt>
2023-04-16 20:37:35 +0100

    Soap servers updated

---
+++ src/sd2223/trab1/servers/soap/SoapFeedsServer.java
@@ -0,0 +1,43 @@
+package sd2223.trab1.servers.soap;
+
+
+import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import jakarta.xml.ws.Endpoint;
+import sd2223.trab1.discovery.Discovery;
+
+public class SoapFeedsServer {
+
+    public static final int PORT = 8081;
+    public static final String SERVICE_NAME = "users";
+    public static String SERVER_BASE_URI = "http://%s:%s/soap";
+
+    private static Logger Log = Logger.getLogger(SoapUsersServer.class.getName());
+
+    public static void main(String[] args) throws Exception {
+
+//		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
+//		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
+//		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
+//		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
+
+        String domain = args[0];
+        long id = Long.parseLong(args[1]);
+
+        Log.setLevel(Level.INFO);
+        try {
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
+            Discovery discovery = Discovery.getInstance();
+            discovery.announce(domain, SERVICE_NAME, serverURI);
+            Endpoint.publish(serverURI.replace(ip, "0.0.0.0"), new SoapFeedsWebService(domain, id));
+
+            Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE_NAME, serverURI));
+
+        } catch (Exception e) {
+            Log.severe(e.getMessage());
+        }
+    }
+}
