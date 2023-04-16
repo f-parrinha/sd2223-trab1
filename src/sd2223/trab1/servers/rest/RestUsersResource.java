6b7f9fa343cfdfef7c89163c8e6a4dc95745cc99
Martin <m.magdalinchev@campus.fct.unl.pt>
2023-04-16 20:37:22 +0100

    Soap servers updated

--- src/sd2223/trab1/servers/rest/RestUsersResource.java
+++ src/sd2223/trab1/servers/rest/RestUsersResource.java
@@ -11,34 +11,34 @@ import jakarta.inject.Singleton;
 @Singleton
 public class RestUsersResource extends RestResource implements UsersService {
 
-	final Users impl;
+    final Users impl;
 
-	public RestUsersResource() {
-		this.impl = new JavaUsers();
-	}
-	
-	@Override
-	public String createUser(User user) {
-		return super.fromJavaResult( impl.createUser( user));
-	}
+    public RestUsersResource() {
+        this.impl = new JavaUsers();
+    }
 
-	@Override
-	public User getUser(String name, String pwd) {
-		return super.fromJavaResult( impl.getUser(name, pwd));
-	}
+    @Override
+    public String createUser(User user) {
+        return super.fromJavaResult(impl.createUser(user));
+    }
 
-	@Override
-	public User updateUser(String name, String pwd, User user) {
-		return super.fromJavaResult(impl.updateUser(name, pwd, user));
-	}
+    @Override
+    public User getUser(String name, String pwd) {
+        return super.fromJavaResult(impl.getUser(name, pwd));
+    }
 
-	@Override
-	public User deleteUser(String name, String pwd) {
-		return super.fromJavaResult(impl.deleteUser(name, pwd));
-	}
+    @Override
+    public User updateUser(String name, String pwd, User user) {
+        return super.fromJavaResult(impl.updateUser(name, pwd, user));
+    }
 
-	@Override
-	public List<User> searchUsers(String pattern) {
-		return super.fromJavaResult(impl.searchUsers(pattern));
-	}
+    @Override
+    public User deleteUser(String name, String pwd) {
+        return super.fromJavaResult(impl.deleteUser(name, pwd));
+    }
+
+    @Override
+    public List<User> searchUsers(String pattern) {
+        return super.fromJavaResult(impl.searchUsers(pattern));
+    }
 }
--- src/sd2223/trab1/servers/soap/SoapUsersServer.java
+++ src/sd2223/trab1/servers/soap/SoapUsersServer.java
@@ -6,29 +6,39 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import jakarta.xml.ws.Endpoint;
+import sd2223.trab1.discovery.Discovery;
 
 public class SoapUsersServer {
 
-	public static final int PORT = 8081;
-	public static final String SERVICE_NAME = "users";
-	public static String SERVER_BASE_URI = "http://%s:%s/soap";
+    public static final int PORT = 8081;
+    public static final String SERVICE_NAME = "users";
+    public static String SERVER_BASE_URI = "http://%s:%s/soap";
 
-	private static Logger Log = Logger.getLogger(SoapUsersServer.class.getName());
+    private static Logger Log = Logger.getLogger(SoapUsersServer.class.getName());
 
-	public static void main(String[] args) throws Exception {
-		
+    public static void main(String[] args) throws Exception {
+
 //		System.setProperty("com.sun.xml.ws.transport.http.client.HttpTransportPipe.dump", "true");
 //		System.setProperty("com.sun.xml.internal.ws.transport.http.client.HttpTransportPipe.dump", "true");
 //		System.setProperty("com.sun.xml.ws.transport.http.HttpAdapter.dump", "true");
 //		System.setProperty("com.sun.xml.internal.ws.transport.http.HttpAdapter.dump", "true");
 
-		Log.setLevel(Level.INFO);
+        String domain = args[0];
 
-		String ip = InetAddress.getLocalHost().getHostAddress();
-		String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
+        Log.setLevel(Level.INFO);
+        try {
+            String ip = InetAddress.getLocalHost().getHostAddress();
+            String serverURI = String.format(SERVER_BASE_URI, ip, PORT);
 
-		Endpoint.publish(serverURI.replace(ip, "0.0.0.0"), new SoapUsersWebService());
+            Discovery discovery = Discovery.getInstance();
+            discovery.announce(domain, SERVICE_NAME, serverURI);
 
-		Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE_NAME, serverURI));
-	}
+            Endpoint.publish(serverURI.replace(ip, "0.0.0.0"), new SoapUsersWebService());
+
+            Log.info(String.format("%s Soap Server ready @ %s\n", SERVICE_NAME, serverURI));
+        } catch (Exception e) {
+            Log.severe(e.getMessage());
+        }
+    }
+
 }
